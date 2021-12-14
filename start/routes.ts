8d8499c9308a7c47cebe8920e249e74c1b977443
MarkAyinomoto <wprakongchai@gmail.com>
2021-12-14 15:18:04 +0700

    Create user controller with resource & route.

--- start/routes.ts
+++ start/routes.ts
@@ -23,3 +23,9 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.get('/', async () => {
   return { hello: 'world' }
 })
+
+Route.group(() => {
+  //user
+  Route.resource('users', 'UsersController').apiOnly()
+
+}).prefix('api')
