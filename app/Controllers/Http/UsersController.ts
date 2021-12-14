bdd433651e97f63c92fcf9d40b6875bfabdbc64f
MarkAyinomoto <wprakongchai@gmail.com>
2021-12-14 16:01:28 +0700

    Store user data.

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,4 +1,5 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
 import User from 'App/Models/User'
 
 export default class UsersController {
@@ -9,7 +10,40 @@ export default class UsersController {
 
   public async create({}: HttpContextContract) {}
 
-  public async store({}: HttpContextContract) {}
+  public async store({ request, response }: HttpContextContract) {
+    const userSchema = schema.create({
+      firstname: schema.string(),
+      lastname: schema.string(),
+      username: schema.string({}, [
+        rules.unique({
+          table: 'users',
+          column: 'username'
+        })
+      ]),
+      password: schema.string({}, [
+        rules.confirmed()
+      ]),
+      date_of_birth: schema.date({
+        format: 'yyyy-MM-dd'
+      })
+    })
+
+    const payload = await request.validate({
+      schema: userSchema,
+      messages: {
+        'required' : '{{ field }} is required.',
+        'unique': 'Username is duplicate, try again.'
+      }
+    })
+
+
+    try{
+      const user = await User.create(payload)
+      return response.status(200).send({result: 'success', data: user})
+    }catch(e){
+      return response.status(500).send({result: 'error', message: e})
+    }
+  }
 
   public async show({}: HttpContextContract) {}
 
--- app/Models/User.ts
+++ app/Models/User.ts
@@ -1,5 +1,6 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, beforeSave, column, hasMany, HasMany, manyToMany, ManyToMany} from '@ioc:Adonis/Lucid/Orm'
+import Hash from '@ioc:Adonis/Core/Hash'
 
 export default class User extends BaseModel {
   @column({ isPrimary: true })
@@ -17,15 +18,25 @@ export default class User extends BaseModel {
   @column()
   public password: string
 
-  @column()
-  public date_of_birth: Date
+  @column.date()
+  public dateOfBirth: DateTime
   
   @column()
   public active: boolean
+
+  @column.dateTime({ autoCreate: true, serialize: (value: DateTime) =>{
+      return value.toFormat('yyyy-mm-dd HH:mm:ss')
+    }
+  })
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  @beforeSave()
+  public static async passwordHashing(user: User){
+    user.password = await Hash.make(user.password)
+  }
 }
