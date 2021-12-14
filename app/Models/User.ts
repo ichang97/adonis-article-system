e142906a083383a57cf010cc45b0e3929af0c922
MarkAyinomoto <wprakongchai@gmail.com>
2021-12-14 14:11:54 +0700

    install auth, create user model with migration.

---
+++ app/Models/User.ts
@@ -0,0 +1,31 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class User extends BaseModel {
+  @column({ isPrimary: true })
+  public id: number
+
+  @column()
+  public firstname: string
+
+  @column()
+  public lastname: string
+
+  @column()
+  public username: string
+
+  @column()
+  public password: string
+
+  @column()
+  public date_of_birth: Date
+  
+  @column()
+  public active: boolean
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
---
+++ database/migrations/1639465267394_users.ts
@@ -0,0 +1,27 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Users extends BaseSchema {
+  protected tableName = 'users'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id').primary()
+      table.string('firstname').notNullable()
+      table.string('lastname').notNullable()
+      table.string('username').notNullable()
+      table.string('password').notNullable()
+      table.date('date_of_birth').notNullable()
+      table.boolean('active').defaultTo(true)
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
