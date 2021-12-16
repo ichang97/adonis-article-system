817680cae54bffad913588064b09c2b7b8bcd05c
MarkAyinomoto <wprakongchai@gmail.com>
2021-12-16 10:16:32 +0700

    install redis library.

--- env.ts
+++ env.ts
@@ -36,4 +36,9 @@ export default Env.rules({
   S3_BUCKET: Env.schema.string(),
   S3_REGION: Env.schema.string(),
   S3_ENDPOINT: Env.schema.string.optional(),
+
+  REDIS_CONNECTION: Env.schema.enum(['local'] as const),
+  REDIS_HOST: Env.schema.string({ format: 'host' }),
+  REDIS_PORT: Env.schema.number(),
+  REDIS_PASSWORD: Env.schema.string.optional(),
 })
