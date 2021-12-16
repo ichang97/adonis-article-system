0a03fd9b581c1a5f69ce2b0da0f6f8bb4011592a
MarkAyinomoto <wprakongchai@gmail.com>
2021-12-16 10:53:53 +0700

    store user data into redis after login.

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,4 +1,5 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Redis from '@ioc:Adonis/Addons/Redis'
 
 export default class AuthController {
     public async login({ auth, request, response }: HttpContextContract){
@@ -9,7 +10,16 @@ export default class AuthController {
         })
 
         if(token){
-            return response.status(200).send({result: 'success', data: {username: username, token: token}});
+            const userData = {
+                username: username,
+                token: token
+            }
+
+            //store user data into redis
+            await Redis.set('USERS_DATA', JSON.stringify(userData))
+            console.log(await Redis.get('USERS_DATA'))
+
+            return response.status(200).send({result: 'success', data: userData});
         }else{
             return response.status(403).send({
                 result: 'error',
@@ -18,8 +28,16 @@ export default class AuthController {
         }
     }
 
-    public async me({ auth, response }: HttpContextContract){
-        return response.status(200).send({data: {user: auth.user} })
+    public async me({ response }: HttpContextContract){
+        const redisData = await Redis.get('USERS_DATA')
+
+        if(redisData){
+            const userData = JSON.parse(redisData)
+            console.log(redisData)
+            return response.status(200).send({data: {user: userData}})
+        }else{
+            return response.status(404).send({result: 'error', message: 'no user data in redis.'})
+        }
     }
 
     public async logout({ auth, response }: HttpContextContract){
